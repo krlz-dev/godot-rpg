b74f068031e1388e6f6362d23c991c7ae1067130
Carlos <inf.carlos@outlook.com>
2022-09-06 00:30:49 +0300

    P17:add health stats to player and bat

--- World/Grass.gd
+++ World/Grass.gd
@@ -3,7 +3,6 @@ extends Node2D
 var grassEffect = preload("res://Effects/GrassEffect.tscn" ).instance()
 
 func create_grass_effect():
-	var world = get_tree().current_scene
 	get_parent().add_child(grassEffect)
 	grassEffect.global_position = global_position
  
